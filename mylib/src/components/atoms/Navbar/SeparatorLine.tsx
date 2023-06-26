a6acd0c7aba0000000313d1b02144d579ea085cf
Joshua Churchill <57601700+JMChurchill@users.noreply.github.com>
2023-06-26 17:40:08 +0100

    Added vertical SeparatorLine

--- mylib/src/components/atoms/Navbar/SeparatorLine.tsx
+++ mylib/src/components/atoms/Navbar/SeparatorLine.tsx
@@ -5,11 +5,30 @@ const SeparatorLine = ({
   children,
   showNav = true,
   noPaddingX = false,
+  isVertical = false,
 }: {
   children?: string;
   showNav?: boolean;
   noPaddingX?: boolean;
+  isVertical?: boolean;
 }) => {
+  if (isVertical)
+    return (
+      <li className="relative flex h-full items-center justify-center">
+        <p
+          className={`z-20 overflow-hidden bg-skin-page-forground px-2 
+        ${showNav ? 'max-w-sm' : 'max-w-0 opacity-0'}
+        transition-all duration-300`}
+        >
+          {children}
+        </p>
+        <div
+          className={`absolute z-0 w-[0.1rem] ${
+            noPaddingX ? 'h-full' : 'h-[90%]'
+          } rounded-xl bg-skin-page-forground-hover`}
+        ></div>
+      </li>
+    );
   return (
     <li className="relative flex w-full items-center justify-center">
       <p
--- mylib/stories/components/atoms/Navbar/SeparatorLine.stories.tsx
+++ mylib/stories/components/atoms/Navbar/SeparatorLine.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 import SeparatorLine from '../../../../src/components/atoms/Navbar/SeparatorLine';
+import Card from '../../../../src/components/atoms/Cards/Card';
 
 const meta: Meta<typeof SeparatorLine> = {
   title: 'Components/atoms/Navbar/SeparatorLine',
@@ -21,4 +22,17 @@ export const NoText: Story = {
 };
 export const NoPadding: Story = {
   args: { noPaddingX: true },
+};
+
+export const Vertical: Story = {
+  decorators: [
+    (Story) => (
+      <Card>
+        <div style={{ height: '20rem', width: '3rem' }}>
+          <Story />
+        </div>
+      </Card>
+    ),
+  ],
+  args: { isVertical: true },
 };
